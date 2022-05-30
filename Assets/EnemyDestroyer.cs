8d3d937d45a30c3b9ba96c5164ba857936c04c35
MohsenMajboor <neilmajboor@gmail.com>
2022-05-29 20:55:56 -0400

    Finished Game

---
+++ Assets/EnemyDestroyer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestroyer : MonoBehaviour
+{
+
+    public event Action onAllEnemiesDead;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var enemy = other.gameObject.GetComponent<EnemyRunFromPlayer>();
+        if(enemy)
+        {
+            Destroy(enemy.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var enemy = other.gameObject.GetComponent<EnemyRunFromPlayer>();
+        if (enemy)
+        {
+            Destroy(enemy.gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var enemy = other.gameObject.GetComponent<EnemyRunFromPlayer>();
+        if (enemy)
+        {
+            Destroy(enemy.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (FindObjectsOfType<EnemyRunFromPlayer>().Length <= 0)
+        {
+            onAllEnemiesDead?.Invoke();
+        }
+    }
+}
+
